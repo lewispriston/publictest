02accee96c7a315c1ad494d0c3fa3d8193f9d306
Priston Lewis <lewispriston>
2021-07-10 21:53:56 +0530

    nodemon to node

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.listen(3000, function() {
-    console.log('listening on port 2424');
+    console.log('listening on port 3000');
 });
 app.get('/', function(req, res) {
     res.send('Hello Priston');
