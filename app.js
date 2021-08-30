b3f8fbb60fd5f1194026ce44974d0d27c0bc8c2b
Priston Lewis <lewispriston>
2021-08-30 23:06:02 +0530

    mongo

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const app = express();
 app.listen(3000, function() {
     console.log('listening on port 3000');
 });
+mongoose.connect('mongodb://localhost:27017/usersdb', {
+    useNewUrlParser: true
+});
+var glob = "hello"
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function() {
+    console.log("Connected successfully");
+    glob = "Connected successfully"
+});
 app.get('/', function(req, res) {
-    res.send('Hello Priston lewis Lissssa');
+    res.send('Hello Priston lewis Lissssa' + glob);
 })
